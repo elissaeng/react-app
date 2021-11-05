b0c18cb73d7db149236b44ecb05989f56183089f
elissaeng <elissaeng@gmail.com>
2021-11-05 15:06:11 -0700

    toggle woggle

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ const App = () => {
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      text: 'blah',
-      day: 'friday',
+      text: 'Biking',
+      day: 'Friday at 11:30am',
       reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Hair Appointment',
+      day: 'Tuesday at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Grocery Shopping',
+      day: 'Monday at 10:30am',
+      reminder: false,
     },
   ])
 
@@ -17,11 +29,21 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    console.log(id);
+  }
+
   return (
     <div className='container'>
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} 
-      onDelete={deleteTask} /> : 'No Tasks To Show'}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+        {deleteTask} onToggle={toggleReminder} /
+        > 
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
   )
 }
