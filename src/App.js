ddfb70330638e02d5f05f18d409fe0ce2b9bedc3
elissaeng <elissaeng@gmail.com>
2021-11-05 14:15:50 -0700

    not working but Ive been working...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
   return (
     <div className='container'>
       <Header />
+      <Tasks />
     </div>
   );
 }
