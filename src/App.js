5d5f7ee6f2509c09280b41fd41a0f462ae5b4ec4
elissaeng <elissaeng@gmail.com>
2021-11-05 14:31:55 -0700

    mo betta

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'blah',
+      day: 'friday',
+      reminder: true,
+    },
+  ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id)
+  }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
-  );
+  )
 }
 
 export default App;
